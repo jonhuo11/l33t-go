a978d028ffb53f7fbaeee848410b044adcb18f2d
Jonathan Huo <jonhuo11@gmail.com>
2024-01-13 16:01:59 -0500

    permutations

--- misc.go
+++ misc.go
@@ -1,24 +1,25 @@
 package main
 
-func PermutationsOfString(s string) []string {
-	out := []string{}
+func permutationsOfString(s string) []string {
+	o := []string{}
 	conv := []rune(s)
 	
-	swap := func(s1 []rune, a, b int) []rune {
+	swap := func(s1 []rune, a, b int) {
 		tmp := s1[a]
 		s1[a] = s1[b]
 		s1[b] = tmp
-		return s1
 	}
-	var h func(str []rune, l int, out []string)
-	h = func(str []rune, l int, out []string) {
-		out = append(out, string(str))
+	var h func(str []rune, l int, out *[]string)
+	h = func(str []rune, l int, out *[]string) {
+		if l == len(str) - 1 {
+			*out = append(*out, string(str))
+		}
 		for i := l; i < len(str); i++ {
 			swap(str, i, l)
-			h(str, i + 1, out)
+			h(str, l + 1, out)
 			swap(str, i, l)
 		}
 	}
-	h(conv, 0, out)
-	return out
+	h(conv, 0, &o)
+	return o
 }
---
+++ misc_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestPermutationsOfString(t *testing.T) {
+	c1 := permutationsOfString("abc")
+	//fmt.Println(c1)
+	c1_expected := []string{"abc", "acb", "bac", "bca", "cab", "cba"}
+	if len(c1) != len(c1_expected) {
+		t.Errorf("Test case 1 failed")
+	}
+	for _, v := range c1 {
+		found := false
+		for _, ve := range c1_expected {
+			if v == ve {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Errorf("Test case 1 failed")
+		}
+	}
+}
